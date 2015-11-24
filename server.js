9cb42984fc5422c04dcdeff8dc937f8fa32455e0
Niccolò Maggioni <nicco.maggioni@gmail.com>
2015-11-24 17:27:31 +0100

    Multiple YubiKey support

--- server.js
+++ server.js
@@ -5,73 +5,96 @@ var express = require("express");
 var app = express();
 var path = require('path');
 var fs = require('fs');
+var yubikey = require('./lib/yubikey.js');
+
+function logWrapper(message) {
+    var timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    winston.log('info', "[" + timestamp + "] %s", message);
+}
+
+/* --- Configuration parsing --- */
+
 var config = JSON.parse(fs.readFileSync('lib/config.js'));
-var yubikey = require('./lib/yubikey.js'); // https://github.com/evilpacket/node-yubikey
+var apiConfigIsArray = false;
+
+if (Object.prototype.toString.call(config.apiId) === '[object Array]') {
+    if (Object.prototype.toString.call(config.apiKey) === '[object Array]') {
+        if (config.apiId.length !== config.apiKey.length) {
+            logWrapper('Configuration mismatch: not the same number of API IDs and Keys.');
+            process.exit(1);
+        }
+    } else {
+        logWrapper('Configuration mismatch: API Keys field is not an array.');
+        process.exit(1);
+    }
+} else {
+    logWrapper('Configuration mismatch: API IDs field is not an array.');
+    process.exit(1);
+}
 yubikey.apiId = config.apiId;
 yubikey.apiKey = config.apiKey;
 
 var uploadPath = config.uploadFolder;
 var domainUrl = config.domainName + uploadPath;
 
-function logWrapper(message) {
-	var timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-	winston.log('info', "[" + timestamp + "] %s", message);
-}
+/* --- End configuration parsing --- */
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-var authUpload = function (req, res, next) {
-  var user = req.query.user;
-  var token = req.query.token;
-  if (user === config.username) {
-    yubikey.verify(token, function(isValid) {
-      if (isValid) {
-        logWrapper("Valid token: " + token);
-        next();
-      } else {
-        logWrapper("Invalid token: " + token);
-        res.status(401).send("AUTH ERROR - Invalid token\n");
-      }
-    });
-  } else {
+var authUpload = function(req, res, next) {
+    var user = req.query.user;
+    var token = req.query.token;
+    if (user === config.username) {
+        yubikey.verify(token, function(isValid, status) {
+            if (isValid) {
+                logWrapper("Valid token: " + token);
+                next();
+            } else {
+                logWrapper("Invalid token: " + token + " (Reason: " + status + ")");
+                res.status(401).send("AUTH ERROR - " + status + "\n");
+            }
+        });
+    } else {
         logWrapper("Invalid user: " + user);
         res.status(401).send("AUTH ERROR - Invalid user\n");
-  }
+    }
 }
 
 var uploadedFileURL = null;
 var upload = multer({
-  dest: uploadPath,
-  rename: function(fieldname, filename) {
-    return filename + Date.now();
-  },
-  limits: {
-    files: 1, // Max 1 file per upload
-    fields: 0, // No extra POST fields
-    fieldSize: config.maxFileSize,
-    fileSize: config.maxFileSize
-  },
-  onFileUploadStart: function(file) {
-	  logWrapper("Accepting upload: " + file.originalname + " (" + file.size + " bytes, "+ file.mimetype + ")");
-	  uploadedFileURL = null;
-  },
-  onFileUploadComplete: function(file) {
-    logWrapper("File '" + file.originalname + "' uploaded to " + file.path);
-    uploadedFileURL = domainUrl + path.basename(file.path);
-  }
-});
+    dest: uploadPath,
+    rename: function(fieldname, filename) {
+        return filename + Date.now();
+    },
+    limits: {
+        fields: 0, // No extra POST fields
+        fieldSize: config.maxFileSize,
+        fileSize: config.maxFileSize
+    },
+    onFileUploadStart: function(file) {
+        logWrapper("Accepting upload: " + file.originalname + " (" + file.size + " bytes, " + file.mimetype + ")");
+        uploadedFileURL = null;
+    },
+    onFileUploadComplete: function(file) {
+        logWrapper("File '" + file.originalname + "' uploaded to " + file.path);
+        uploadedFileURL = domainUrl + path.basename(file.path);
+    }
+}).single('file');
 
-var postUpload = function (req, res) {
+var postUpload = function(req, res) {
     res.end("OK - " + uploadedFileURL + "\n");
 }
 
-app.post('/api/upload', [authUpload, upload, postUpload]);
+app.post('/api/upload', authUpload, upload, postUpload);
 
 app.get('/uploads/*', function(req, res) {
-  res.sendFile(__dirname + req.url);
+    res.sendFile(__dirname + req.url);
 });
 
 app.listen(9980, function() {
-  logWrapper("Starting Dumpster on port 9980");
+    logWrapper("Starting Dumpster on port 9980");
 });
+
